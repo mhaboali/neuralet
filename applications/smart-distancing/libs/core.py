b20d641e7cfd7ba0b8a1adf40041cf5d030b76dd
mhaboali <m.aboali1117@gmail.com>
2020-07-03 13:37:05 +0100

    After adding the gstreamer function

--- applications/smart-distancing/libs/core.py
+++ applications/smart-distancing/libs/core.py
@@ -73,8 +73,37 @@ class Distancing:
         objects_list, distancings = self.calculate_distancing(tmp_objects_list)
         return cv_image, objects_list, distancings
 
+    def gstreamer_pipeline(
+        self,
+        capture_width=1280,
+        capture_height=720,
+        display_width=1280,
+        display_height=720,
+        framerate=60,
+        flip_method=0,
+    ):
+        return (
+            "nvarguscamerasrc ! "
+            "video/x-raw(memory:NVMM), "
+            "width=(int)%d, height=(int)%d, "
+            "format=(string)NV12, framerate=(fraction)%d/1 ! "
+            "nvvidconv flip-method=%d ! "
+            "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+            "videoconvert ! "
+            "video/x-raw, format=(string)BGR ! appsink"
+            % (
+                capture_width,
+                capture_height,
+                framerate,
+                flip_method,
+                display_width,
+                display_height,
+            )
+        )
+
+
     def process_video(self, video_uri):
-        input_cap = cv.VideoCapture(video_uri)
+        input_cap = cv.VideoCapture(self.gstreamer_pipeline(flip_method=0), cv.CAP_GSTREAMER)
 
         if (input_cap.isOpened()):
             print('opened video ', video_uri)
